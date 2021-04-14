4af0320ad4c72cf15da85e8f6181a575cfe936d6
KeletMada <mileaddy@gmail.com>
2021-04-14 16:08:37 +0200

    beolvasas

---
+++ Nobel/Dijazott.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel
+{
+    class Dijazott
+    {
+        public int Evszam { get; }
+        public string Tipus { get; }
+        public string Keresztnev { get; }
+        public string Vezeteknev { get; }
+
+        public Dijazott(string sor)
+        {
+            string[] s = sor.Split(';');
+            Evszam = int.Parse(s[0]);
+            Tipus = s[1];
+            Keresztnev = s[2];
+            Vezeteknev = s[3];
+        }
+    }
+}
--- Nobel/Program.cs
+++ Nobel/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,18 @@ namespace Nobel
     {
         static void Main(string[] args)
         {
+            //2
+
+            List<Dijazott> dijasok = new List<Dijazott>();
+            string[] sorok = File.ReadAllLines("nobel.csv");
+            foreach  (string sor in sorok.Skip(1))
+            {
+                dijasok.Add(new Dijazott(sor));
+            }
+
+
+
+            Console.ReadLine();
         }
     }
 }
